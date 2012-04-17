b83e5eb9dc06014cb398c885eeee991ad5aca41c
lambje <lambje@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-17 23:31:28 +0000

    v1.4.4 changes - version update (missed :( )
    git-svn-id: https://plugins.svn.wordpress.org/webphysiology-portfolio/trunk@532600 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- portfolio-main.php
+++ portfolio-main.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WEBphysiology Portfolio
 Plugin URI: http://webphysiology.com/redir/webphysiology-portfolio/
-Version: 1.4.3
+Version: 1.4.4
 Description: Provides a clean Portfolio listing with image, details and portfolio type taxonomy. A [portfolio] shortcode is used to include the portfolio on any page.
 Author: Jeff Lambert
 Author URI: http://webphysiology.com/redir/webphysiology-portfolio/author/
